03194a3e6051da68498350bf39917e12dc6035d5
Ekramvis <ekramvis@gmail.com>
2013-08-10 22:32:07 -0700

    added localdate helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def localdate
+    Time.now.in_time_zone.to_date
+  end
+
+  def date_formatted(sub = 0)
+    (localdate - sub.days).strftime("%a %b ") + (localdate - sub.days).strftime("%d").to_i.ordinalize
+  end
 end
